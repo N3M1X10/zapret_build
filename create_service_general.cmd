97336065018ee4621b212571bbb6e22fc42bd4a0
N3M1X10 <nemixmail@gmail.com>
2024-10-09 23:03:24 +0500

    удалил вообще все примочки

--- create_service_general.cmd
+++ create_service_general.cmd
@@ -1,23 +1,6 @@
 @echo off
 chcp 65001 >nul
 
-rem :: Проверка пути
-rem set scriptPath=%~dp0
-rem set "path_no_spaces=%scriptPath: =%"
-rem if not "%scriptPath%"=="%path_no_spaces%" (
-rem     echo Путь содержит пробелы. 
-rem     echo Пожалуйста, переместите скрипт в папку без пробелов.
-rem     >nul pause
-rem     exit /b
-rem )
-
-:: Наличие прав администратора
-echo.
-echo Данный файл должен быть запущен с правами администратора (ПКМ -^> Запустить от имени администратора).
-echo Если вы уверены, нажмите любую клавишу, чтобы продолжить создание службы
-echo.
-pause
-
 
 set BIN=%~dp0zapret\zapret-winws\
 set ARGS=--wf-tcp=443-65535 --wf-udp=443-65535 ^
