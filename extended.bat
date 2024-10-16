9e0fe4bd4dffa06348b1f42782b78dc89d46db7c
N3M1X10 <nemixmail@gmail.com>
2024-10-16 18:06:53 +0500

    extended fix

--- extended.bat
+++ extended.bat
@@ -1,6 +1,6 @@
 cd /d "%~dp0"
 set BIN=%~dp0zapret\zapret-winws\
-start "zapret: general" /min "%BIN%winws.exe" ^
+start "zapret: extended" /min "%BIN%winws.exe" --wf-raw="@lists\rules.txt" ^
 --wf-tcp=80,443 --wf-udp=443,50000-65535 ^
 --filter-udp=443 --hostlist="lists\list-extended.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-udplen-increment=10 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
 --filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
